6d381359b9d7dbe2f26ffcd1632b39aaae90d7b0
Flav-legrand <mongouoflav@gmail.com>
2026-05-11 10:25:41 +0100

    Deploy: Fresh web build with Vos accès rapides updates

--- docs/main.dart.js
+++ docs/main.dart.js
@@ -25159,7 +25159,7 @@ $S:2}
 A.akf.prototype={
 $1(a){var s=A.cL().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/59aa584fdf100e6c78c785d8a5b565d1de4b48ab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/42d3d75a56efe1a2e9902f52dc8006099c45d937/":s)+a},
 $S:122}
 A.Ia.prototype={
 gv(a){var s=this.a
